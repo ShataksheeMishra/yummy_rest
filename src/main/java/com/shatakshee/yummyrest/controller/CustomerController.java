c733781a1b29132171600544f1d84fdbbb411bc9
ShataksheeMishra <shatakshee.mishra@gmail.com>
2024-11-20 00:02:29 +0530

    login

--- src/main/java/com/shatakshee/yummyrest/controller/CustomerController.java
+++ src/main/java/com/shatakshee/yummyrest/controller/CustomerController.java
@@ -1,6 +1,8 @@
 package com.shatakshee.yummyrest.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.shatakshee.yummyrest.dto.CustomerRequest;
+import com.shatakshee.yummyrest.dto.CustomerResponse;
 import com.shatakshee.yummyrest.service.CustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +13,28 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
+
     private final CustomerService customerService;
 
     @PostMapping
     public ResponseEntity<String> createCustoemr(@RequestBody @Valid CustomerRequest request) {
         return ResponseEntity.ok(customerService.createCustomer(request));
     }
+    @PostMapping("/update")
+    public ResponseEntity<String> updateCustomer(@RequestBody JsonNode requestBody) {
+        return ResponseEntity.ok(customerService.updateCustomer(requestBody));
+    }
 
+    @GetMapping("/{email}")
+    public ResponseEntity<CustomerResponse> getCustomer(@PathVariable("email") String email) {
+        return ResponseEntity.ok(customerService.retrieveCustomer(email));
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable("email") String email) {
+        email = email.trim();
+        customerService.deleteCustomer(email);
+        return ResponseEntity.ok("Customer account deleted successfully");
+
+    }
 }
